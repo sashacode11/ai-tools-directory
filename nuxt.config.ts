23751786c084f399bb60e603e980e10958900bb0
Sasha <chan.sasha16@gmail.com>
2026-06-23 11:49:40 +0700

    added google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,6 +45,16 @@ export default defineNuxtConfig({
           src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2075457485171732',
           crossorigin: 'anonymous',
         },
+        {
+          async: true,
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-SC3Q5WE1NN',
+        },
+        {
+          children: `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-SC3Q5WE1NN');`,
+        },
       ],
     },
   },
